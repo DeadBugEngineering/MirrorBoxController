1cb7ba2f4717a8b381160219af00597a6e18c3be
DeadBugEngineering <johannes.treu@gmail.com>
2016-01-06 14:33:47 +0100

    still sandboxing

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from myhdl import *
 
+# definition of constants / configuration parameters
+c_not_reached = 0
+c_reached = 1
+c_direction_pos1 = 0
+c_direction_pos2 = 1
+c_position_pos1 = 0
+c_position_pos2 = 1
+c_speed_slow = 0
+c_speed_fast = 1
+c_pos1_seeking_fast_counter_maxvalue = 777
+c_pos1_seeking_slow_counter_maxvalue = 999
+c_pos2_seeking_fast_counter_maxvalue = 30
+c_pos2_seeking_slow_counter_maxvalue = 23
+c_microsteps_per_seconds_fast = 100
+c_microsteps_per_seconds_slow = 20
+
+
 m_state = enum('undefined',
                'init',
                'seek_home',
@@ -19,7 +36,7 @@ def MirrorBoxController(
         clk,                # input, main clock
         reset,              # input, main reset, active low
         state_reset,        # input, synchronous reset for the FSM state, active low
-        state,              # output, state of the FSM
+        state,              # output, state of the FSM for debugging purposes
         hall1_not,          # input, raw signal of the hallsensor on position 1, active low
         hall2_not,          # input, raw signal of the hallsensor on position 2, active low
         drive2pos1_manual,  # input, driving this signal high sets the target_position to pos1, manual operation
@@ -28,22 +45,8 @@ def MirrorBoxController(
         drive2pos2_PIO,     # input, driving this signal high sets the target_position to pos2, PIO interface
         lock_manual_input,  # input, driving this signal high disables the manual operation
         stepper_direction,  # output, direction signal for the stepper driver, 1==CW, 0==CCW
-        stepper_steps,      # output, step signal for the stepper driver, each cycle equals one microstep
-        osidjoid
+        stepper_steps      # output, step signal for the stepper driver, each cycle equals one microstep
 ):
-
-    # definition of configuration parameters
-    seek_home_counter_maxvalue = Signal(555)
-    pos1_seeking_fast_counter_maxvalue = Signal(555)
-    pos1_seeking_slow_counter_maxvalue = Signal(555)
-    pos2_seeking_fast_counter_maxvalue = Signal(555)
-    pos2_seeking_slow_counter_maxvalue = Signal(555)
-    steps_per_second_fast = Signal(555)
-    steps_per_second_slow = Signal(555)
-    cw_direction = Signal(1)
-
-
-
 
     # definition of internal registers
     target_position = Signal(bool(0))   # 0==Pos1, 1==Pos2
@@ -53,44 +56,63 @@ def MirrorBoxController(
     reg_pos2_seeking_slow_counter = Signal(0)
 
     # definition of internal flags
+    flag_stepper_direction = Signal(bool(0))
     flag_stepper_enable = Signal(bool(0))   # enables the generation of the stepping signal for the stepper driver
-    flag_stepper_speed = Signal(bool(0)     # 0==slow stepping speed, 1==fast stepping speed
+    flag_stepper_speed = Signal(bool(0))     # 0==slow stepping speed, 1==fast stepping speed
     flag_seek_home_counter_top = Signal(bool(0))
     flag_seek_pos1_slow_counter_top = Signal(bool(0))    # 1 when seeking position 1 goes into timeout
     flag_seek_pos1_fast_counter_top = Signal(bool(0))    # 1 when time for fast seeking is up
     flag_seek_pos2_slow_counter_top = Signal(bool(0))    # 1 when seeking position 2 goes into timeout
     flag_seek_pos2_fast_counter_top = Signal(bool(0))    # 1 when time for fast seeking is up
 
+    # definition of internal signals
+    hall1 = Signal(bool(0))     # 1 when the magnet has reached hallsensor 1, active high
+    hall2 = Signal(bool(0))     # 1 when the magnet has reached hallsensor 2, active high
 
 
+    @always_comb
+    def inverter_hall1():
+        hall1.next = not hall1_not
+
+    @always_comb
+    def inverter_hall2():
+        hall2.next = not hall2_not
 
 
     @always_seq(clk.posedge, reset=reset)
     def FSM():
         if state == m_state.init:
             state.next = m_state.seek_home
+            flag_stepper_direction.next = Signal(bool(c_direction_home))
+            flag_stepper_speed.next = Signal(bool(c_speed_slow))
+            flag_stepper_enable.next = Signal(bool(0))
 
         elif state == m_state.seek_home:
-            if seek_home_counter_top == Signal(bool(1)):
+            flag_stepper_enable.next = Signal(bool(1))
+            if flag_seek_home_counter_top == Signal(bool(c_reached)):
                 state.next = m_state.seek_home_timeout
-
-            elif (hall1 == Signal(bool(1)) and hall2 == Signal(bool(0))):
-                state.next = m_state.pos1_resting
-
             else:
-                state.next = m_state.seek_home
+                if hall1 == Signal(bool(c_reached)) and hall2 == Signal(bool(c_not_reached)):
+                    state.next = m_state.pos1_resting
+                else:
+                    state.next = m_state.seek_home
 
         elif state == m_state.seek_home_timeout:
-            state.next = m_state.seek_home_timeout
+            flag_stepper_enable.next = Signal(bool(0))
+            if state_reset == Signal(bool(1)):
+                state.next = m_state.init
+            else:
+                state.next = m_state.seek_home_timeout
 
         elif state == m_state.pos1_resting:
-            if target_position == Signal(bool(1)):
+            flag_stepper_enable.next = Signal(bool(0))
+            if target_position == Signal(bool(target_position)):
                 state.next = m_state.pos2_seeking_fast
             else:
                 if hall1 == Signal(bool(0)):
                     state.next = m_state.pos1_resting
                 else:
-                    state.next = pos1_resting_error
+                    state.next = m_state.pos1_resting_error
 
         elif state == m_state.pos2_resting:
             if target_position == Signal(bool(0)):
@@ -99,7 +121,7 @@ def MirrorBoxController(
                 if hall2 == Signal(bool(0)):
                     state.next = m_state.pos2_resting
                 else:
-                    state.next = pos2_resting_error
+                    state.next = m_state.pos2_resting_error
 
         elif state == m_state.pos1_resting_error:
             state.next = m_state.pos1_resting_error
@@ -132,9 +154,18 @@ def MirrorBoxController(
                 state.next = m_state.pos2_seeking_fast
 
         elif state == m_state.pos1_seeking_timeout:
+            if state_reset == Signal(bool(1)):
+                state.next = m_state.init
+            else:
+                state.next = m_state.pos1_seeking_timeout
 
         elif state == m_state.pos2_seeking_timeout:
+            if state_reset == Signal(bool(1)):
+                state.next = m_state.init
+            else:
+                state.next = m_state.pos2_seeking_timeout
 
+    return FSM, inverter_hall1, inverter_hall2
 
 
 
