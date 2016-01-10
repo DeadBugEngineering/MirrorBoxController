86054829601ff18cf701c8026b09c23d42868ef3
DeadBugEngineering <johannes.treu@gmail.com>
2016-01-10 17:33:26 +0100

    sandboxing almost done, todo: fiddeling with constants and generating verilog code

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ c_position_pos1 = 0
 c_position_pos2 = 1
 c_speed_slow = 0
 c_speed_fast = 1
+c_home_seeking_slow_counter_maxvalue = 9
 c_pos1_seeking_fast_counter_maxvalue = 10
 c_pos1_seeking_slow_counter_maxvalue = 11
 c_pos2_seeking_fast_counter_maxvalue = 12
 c_pos2_seeking_slow_counter_maxvalue = 13
-c_microsteps_per_seconds_fast = 100
-c_microsteps_per_seconds_slow = 20
-
+c_clock_freq = 10000000
+c_microsteps_per_seconds_fast = 10000000
+c_microsteps_per_seconds_slow = 10000000
+c_prescaler_slow = c_clock_freq / c_microsteps_per_seconds_slow
+c_prescaler_fast = c_clock_freq / c_microsteps_per_seconds_fast
 
 m_state = enum('undefined',
                'init',
@@ -50,21 +53,25 @@ def mirror_box_controller(
 ):
 
     # definition of internal registers
-    target_position = Signal(bool(0))   # 0==Pos1, 1==Pos2
+    target_position =  Signal(bool(c_position_pos1))  # 0==Pos1, 1==Pos2
+    reg_home_seeking_slow_counter = Signal(0)
     reg_pos1_seeking_fast_counter = Signal(0)
     reg_pos1_seeking_slow_counter = Signal(0)
     reg_pos2_seeking_fast_counter = Signal(0)
     reg_pos2_seeking_slow_counter = Signal(0)
+    reg_step_generator_slow_counter = Signal(0)
+    reg_step_generator_fast_counter = Signal(0)
 
     # definition of internal flags
     flag_stepper_direction = Signal(bool(0))
     flag_stepper_enable = Signal(bool(0))   # enables the generation of the stepping signal for the stepper driver
     flag_stepper_speed = Signal(bool(0))     # 0==slow stepping speed, 1==fast stepping speed
-    flag_seek_home_counter_top = Signal(bool(0))
+    flag_seek_home_slow_enable = Signal(bool(0))
     flag_seek_pos1_slow_enable = Signal(bool(0))
     flag_seek_pos1_fast_enable = Signal(bool(0))
     flag_seek_pos2_slow_enable = Signal(bool(0))
     flag_seek_pos2_fast_enable = Signal(bool(0))
+    flag_seek_home_slow_counter_top = Signal(bool(0))
     flag_seek_pos1_slow_counter_top = Signal(bool(0))    # 1 when seeking position 1 goes into timeout
     flag_seek_pos1_fast_counter_top = Signal(bool(0))    # 1 when time for fast seeking is up
     flag_seek_pos2_slow_counter_top = Signal(bool(0))    # 1 when seeking position 2 goes into timeout
@@ -73,6 +80,38 @@ def mirror_box_controller(
     # definition of internal signals
     hall1 = Signal(bool(0))     # 1 when the magnet has reached hallsensor 1, active high
     hall2 = Signal(bool(0))     # 1 when the magnet has reached hallsensor 2, active high
+    step_clock_slow = Signal(bool(0))    # clock for the stepper driver when seeking slowly
+    step_clock_fast = Signal(bool(0))    # clock for the stepper driver when seeking fast
+
+    @always_seq(clk.posedge, reset=reset)
+    def step_generator_slow():
+        if reg_step_generator_slow_counter <= Signal((c_prescaler_slow -2) / 2):
+            reg_step_generator_slow_counter.next = reg_step_generator_slow_counter + 1
+        else:
+            reg_step_generator_slow_counter.next = Signal(0)
+            step_clock_slow.next = not step_clock_slow
+
+    @always_seq(clk.posedge, reset=reset)
+    def step_generator_fast():
+        if reg_step_generator_fast_counter <= Signal((c_prescaler_fast -2) / 2):
+            reg_step_generator_fast_counter.next = reg_step_generator_fast_counter + 1
+        else:
+            reg_step_generator_fast_counter.next = Signal(0)
+            step_clock_fast.next = not step_clock_fast
+
+    @always_comb
+    def step_output():
+        if flag_stepper_enable == Signal(bool(1)):
+            if flag_stepper_speed == Signal(bool(c_speed_fast)):
+                stepper_steps.next = step_clock_fast
+            else:
+                stepper_steps.next = step_clock_slow
+        else:
+            stepper_steps.next = Signal(bool(0))
+
+    @always_comb
+    def generate_stepper_direction():
+        stepper_direction.next = flag_stepper_direction
 
     @always_comb
     def inverter_hall1():
@@ -92,8 +131,33 @@ def mirror_box_controller(
             target_position.next = Signal(bool(c_position_pos2))
         elif drive2pos2_PIO == Signal(bool(1)):
             target_position.next = Signal(bool(c_position_pos2))
-        else:
-            target_position.next = Signal(bool(c_position_pos1))
+
+    @always_seq(clk.posedge, reset=reset)
+    def home_seeking_slow_counter():
+        if flag_seek_home_slow_enable == Signal(bool(1)):
+            if reg_home_seeking_slow_counter > c_home_seeking_slow_counter_maxvalue:
+                flag_seek_home_slow_counter_top.next = Signal(bool(1))
+            else:
+                flag_seek_home_slow_counter_top.next = Signal(bool(0))
+                reg_home_seeking_slow_counter.next = reg_home_seeking_slow_counter + 1
+
+    @always_seq(clk.posedge, reset=reset)
+    def pos1_seeking_slow_counter():
+        if flag_seek_pos1_slow_enable == Signal(bool(1)):
+            if reg_pos1_seeking_slow_counter > c_pos1_seeking_slow_counter_maxvalue:
+                flag_seek_pos1_slow_counter_top.next = Signal(bool(1))
+            else:
+                flag_seek_pos1_slow_counter_top.next = Signal(bool(0))
+                reg_pos1_seeking_slow_counter.next = reg_pos1_seeking_slow_counter + 1
+
+    @always_seq(clk.posedge, reset=reset)
+    def pos1_seeking_fast_counter():
+        if flag_seek_pos1_fast_enable == Signal(bool(1)):
+            if reg_pos1_seeking_fast_counter > c_pos1_seeking_fast_counter_maxvalue:
+                flag_seek_pos1_fast_counter_top.next = Signal(bool(1))
+            else:
+                flag_seek_pos1_fast_counter_top.next = Signal(bool(0))
+                reg_pos1_seeking_fast_counter.next = reg_pos1_seeking_fast_counter + 1
 
     @always_seq(clk.posedge, reset=reset)
     def pos2_seeking_slow_counter():
@@ -120,20 +184,22 @@ def mirror_box_controller(
             flag_stepper_direction.next = Signal(bool(c_direction_pos1))
             flag_stepper_speed.next = Signal(bool(c_speed_slow))
             flag_stepper_enable.next = Signal(bool(0))
-            flag_seek_home_counter_top.next = Signal(bool(0))
+            flag_seek_home_slow_counter_top.next = Signal(bool(0))
             flag_seek_pos1_slow_counter_top.next = Signal(bool(0))
             flag_seek_pos1_fast_counter_top.next = Signal(bool(0))
             flag_seek_pos2_slow_counter_top.next = Signal(bool(0))
             flag_seek_pos2_fast_counter_top.next = Signal(bool(0))
             target_position.next = Signal(bool(c_position_pos1))
+            reg_home_seeking_slow_counter.next = Signal(0)
             reg_pos1_seeking_fast_counter.next = Signal(0)
             reg_pos1_seeking_slow_counter.next = Signal(0)
             reg_pos2_seeking_fast_counter.next = Signal(0)
             reg_pos2_seeking_slow_counter.next = Signal(0)
 
         elif state == m_state.seek_home:
+            flag_seek_home_slow_enable.next = Signal(bool(1))
             flag_stepper_enable.next = Signal(bool(1))
-            if flag_seek_home_counter_top == Signal(bool(c_reached)):
+            if flag_seek_home_slow_counter_top == Signal(bool(c_reached)):
                 state.next = m_state.seek_home_timeout
             else:
                 if hall1 == Signal(bool(c_reached)) and hall2 == Signal(bool(c_not_reached)):
@@ -142,6 +208,7 @@ def mirror_box_controller(
                     state.next = m_state.seek_home
 
         elif state == m_state.seek_home_timeout:
+            flag_seek_home_slow_enable.next = Signal(bool(0))
             flag_stepper_enable.next = Signal(bool(0))
             if state_reset == Signal(bool(1)):
                 state.next = m_state.init
@@ -149,6 +216,10 @@ def mirror_box_controller(
                 state.next = m_state.seek_home_timeout
 
         elif state == m_state.pos1_resting:
+            reg_home_seeking_slow_counter.next = Signal(0)
+            reg_pos1_seeking_slow_counter.next = Signal(0)
+            flag_seek_home_slow_enable.next = Signal(bool(0))
+            flag_seek_pos1_slow_enable.next = Signal(bool(0))
             flag_stepper_enable.next = Signal(bool(0))
             if target_position == Signal(bool(c_position_pos2)):
                 state.next = m_state.pos2_seeking_fast
@@ -163,6 +234,8 @@ def mirror_box_controller(
                     state.next = m_state.pos1_resting_error
 
         elif state == m_state.pos2_resting:
+            reg_pos2_seeking_slow_counter.next = Signal(0)
+            flag_seek_pos2_slow_enable.next = Signal(bool(0))
             flag_stepper_enable.next = Signal(bool(0))
             if target_position == Signal(bool(c_position_pos1)):
                 state.next = m_state.pos1_seeking_fast
@@ -177,18 +250,23 @@ def mirror_box_controller(
                     state.next = m_state.pos2_resting_error
 
         elif state == m_state.pos1_resting_error:
+            flag_stepper_enable.next = Signal(bool(0))
             if state_reset == Signal(bool(1)):
                 state.next = m_state.init
             else:
                 state.next = m_state.pos1_resting_error
 
         elif state == m_state.pos2_resting_error:
+            flag_stepper_enable.next = Signal(bool(0))
             if state_reset == Signal(bool(1)):
                 state.next = m_state.init
             else:
                 state.next = m_state.pos2_resting_error
 
         elif state == m_state.pos1_seeking_slow:
+            flag_seek_pos1_fast_enable.next = Signal(bool(0))
+            flag_seek_pos1_slow_enable.next = Signal(bool(1))
+            reg_pos1_seeking_fast_counter.next = Signal(0)
             flag_stepper_direction.next = Signal(bool(c_direction_pos1))
             flag_stepper_speed.next = Signal(bool(c_speed_slow))
             flag_stepper_enable.next = Signal(bool(1))
@@ -201,6 +279,8 @@ def mirror_box_controller(
                     state.next = m_state.pos1_seeking_slow
 
         elif state == m_state.pos2_seeking_slow:
+            flag_stepper_enable.next = Signal(bool(1))
+            reg_pos2_seeking_fast_counter.next = Signal(0)
             flag_stepper_direction.next = Signal(bool(c_direction_pos2))
             flag_stepper_speed.next = Signal(bool(c_speed_slow))
             flag_seek_pos2_fast_enable.next = Signal(bool(0))
@@ -214,6 +294,11 @@ def mirror_box_controller(
                     state.next = m_state.pos2_seeking_slow
 
         elif state == m_state.pos1_seeking_fast:
+            flag_stepper_enable.next = Signal(bool(1))
+            flag_stepper_enable.next = Signal(bool(1))
+            flag_stepper_direction.next = Signal(bool(c_direction_pos1))
+            flag_stepper_speed.next = Signal(bool(c_speed_fast))
+            flag_seek_pos1_fast_enable.next = Signal(bool(1))
             if flag_seek_pos1_fast_counter_top == Signal(bool(1)):
                 reg_pos1_seeking_slow_counter.next = Signal(0)
                 state.next = m_state.pos1_seeking_slow
@@ -224,6 +309,7 @@ def mirror_box_controller(
                 state.next = m_state.pos1_seeking_fast
 
         elif state == m_state.pos2_seeking_fast:
+            flag_stepper_enable.next = Signal(bool(1))
             flag_seek_pos2_fast_enable.next = Signal(bool(1))
             flag_seek_pos2_slow_enable.next = Signal(bool(0))
             if flag_seek_pos2_fast_counter_top == Signal(bool(1)):
@@ -236,12 +322,14 @@ def mirror_box_controller(
                 state.next = m_state.pos2_seeking_fast
 
         elif state == m_state.pos1_seeking_timeout:
+            flag_stepper_enable.next = Signal(bool(0))
             if state_reset == Signal(bool(1)):
                 state.next = m_state.init
             else:
                 state.next = m_state.pos1_seeking_timeout
 
         elif state == m_state.pos2_seeking_timeout:
+            flag_stepper_enable.next = Signal(bool(0))
             if state_reset == Signal(bool(1)):
                 state.next = m_state.init
             else:
@@ -249,9 +337,11 @@ def mirror_box_controller(
         else:
             state.next = m_state.init
 
-    return fsm, inverter_hall1, inverter_hall2, pos2_seeking_fast_counter,\
-           pos2_seeking_slow_counter, update_target_position
-
+    # return fsm, inverter_hall1, inverter_hall2, pos1_seeking_fast_counter, pos2_seeking_fast_counter,\
+    #        pos1_seeking_slow_counter. pos2_seeking_slow_counter, home_seeking_slow_counter, update_target_position
+    return fsm, inverter_hall1, inverter_hall2, pos1_seeking_fast_counter, pos2_seeking_fast_counter,\
+           pos1_seeking_slow_counter, home_seeking_slow_counter, update_target_position, pos2_seeking_slow_counter,\
+            step_generator_slow, step_generator_fast, step_output, generate_stepper_direction
 
 def testbench():
     clk = Signal(bool(0))
@@ -260,12 +350,12 @@ def testbench():
     state = Signal(m_state.init)
     hall1_not = Signal(bool(1))
     hall2_not = Signal(bool(1))
-    drive2pos1_manual = Signal(0)
-    drive2pos2_manual = Signal(0)
-    drive2pos1_PIO = Signal(0)
-    drive2pos2_PIO = Signal(0)
-    lock_manual_input = Signal(0)
-    stepper_direction = Signal(c_direction_pos1)
+    drive2pos1_manual = Signal(bool(0))
+    drive2pos2_manual = Signal(bool(0))
+    drive2pos1_PIO = Signal(bool(0))
+    drive2pos2_PIO = Signal(bool(0))
+    lock_manual_input = Signal(bool(0))
+    stepper_direction = Signal(bool(c_direction_pos1))
     stepper_steps = Signal(bool(0))
 
     dut = mirror_box_controller(clk, reset, state_reset, state, hall1_not, hall2_not, drive2pos1_manual,
@@ -278,7 +368,7 @@ def testbench():
 
     @instance
     def stimulus_clock():
-        for i in range(3000):
+        for i in range(5000):
             yield clk.posedge
         raise StopSimulation
 
@@ -288,13 +378,32 @@ def testbench():
         hall1_not.next = Signal(bool(0))
         yield delay(900)
         hall1_not.next = Signal(bool(1))
+        yield delay(11700)
+        hall1_not.next = Signal(bool(0))
+        yield delay(1000)
+        hall1_not.next = Signal(bool(1))
+        yield delay(5500)
+        hall1_not.next = Signal(bool(0))
 
     @instance
     def stim_hall2_not():
         yield delay(4000)
         hall2_not.next = Signal(bool(0))
-        yield delay(5000)
+        yield delay(2000)
         hall2_not.next = Signal(bool(1))
+        yield delay(10000)
+        hall2_not.next = Signal(bool(0))
+
+    @instance
+    def stim_state_reset():
+        yield delay(9000)
+        state_reset.next = Signal(bool(1))
+        yield delay(1000)
+        state_reset.next = Signal(bool(0))
+        yield delay(2000)
+        state_reset.next = Signal(bool(1))
+        yield delay(1000)
+        state_reset.next = Signal(bool(0))
 
     @instance
     def stim_drive2pos2_man():
@@ -303,13 +412,27 @@ def testbench():
         yield delay(100)
         drive2pos2_manual.next = Signal(bool(0))
 
+    @instance
+    def stim_drive2pos2_PIO():
+        yield delay(13500)
+        drive2pos2_PIO.next = Signal(bool(1))
+        yield delay(500)
+        drive2pos2_PIO.next = Signal(bool(0))
+
+    @instance
+    def stim_drive2pos1_man():
+        yield delay(17000)
+        drive2pos1_manual.next = Signal(bool(1))
+        yield delay(1000)
+        drive2pos1_manual.next = Signal(bool(0))
 
     @always_seq(clk.posedge, reset=reset)
     def output_printer():
         print now(), state
 
     #return dut, clkgen, stimulus, output_printer
-    return dut, clkgen, stimulus_clock, stim_hall1_not, stim_hall2_not, output_printer, stim_drive2pos2_man
+    return dut, clkgen, stimulus_clock, stim_hall1_not, stim_hall2_not, output_printer, stim_drive2pos2_man,\
+        stim_state_reset, stim_drive2pos2_PIO, stim_drive2pos1_man
 
 tb_fsm = traceSignals(testbench)
 sim = Simulation(tb_fsm)
